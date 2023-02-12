3e36ba33edc6ca744ac8bcefa4fd425f60349e3c
pythagoras58 <solomondanso58@gmail.com>
2023-02-12 16:52:24 +0000

    Final

--- src/test/java/GraderTest.java
+++ src/test/java/GraderTest.java
@@ -16,4 +16,23 @@ public class GraderTest extends TestCase {
         var grader = new Grader();
         assertEquals('A', grader.determineLetterGrade(98));
     }
+
+    public void testDetermineLetterGradeReturnB(){
+        // arrange
+        var grader = new Grader();
+        assertEquals('B', grader.determineLetterGrade(88));
+    }
+
+    public void testDetermineLetterGradeReturnC(){
+        // arrange
+        var grader = new Grader();
+        assertEquals('C', grader.determineLetterGrade(78));
+    }
+
+    public void testDetermineLetterGradeReturnIllegalArgumentException(){
+        // arrange
+        var grader = new Grader();
+
+
+    }
 }
